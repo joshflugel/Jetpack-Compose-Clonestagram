2289b72445113ef0e9bb7fdcbc0324a9aece8be7
Josh Flugel <flugeljosh@gmail.com>
2025-02-27 23:11:34 +0100

    Add Personalized TextFields, password visibility logic

--- app/src/main/java/com/josh25/jetpackcomposeclonestagram/MainActivity.kt
+++ app/src/main/java/com/josh25/jetpackcomposeclonestagram/MainActivity.kt
@@ -18,12 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetpackComposeClonestagramTheme {
+            JetpackComposeClonestagramTheme(darkTheme = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(Modifier.padding(innerPadding))
                 }
             }
         }
